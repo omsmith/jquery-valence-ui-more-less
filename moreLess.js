3d342f8823b4cf7a264080d5f2ecfb1706f25bc5
Rylan Cottrell <Rylan.Cottrell@Desire2Learn.com>
2013-10-22 11:39:03 -0400

    Stubs for more less control

---
+++ moreLess.js
@@ -0,0 +1,21 @@
+﻿( function( $ ) {
+
+	$.widget( "vui.vui_moreLess", {
+
+
+
+		_create: function() {
+
+		},
+
+		_destroy: function() {
+
+		}
+
+	} );
+
+	vui.addClassInitializer(
+
+	);
+
+} )( window.jQuery );
