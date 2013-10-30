521b26618a75cc9dbd1666d31da6a510a42dab69
Rylan Cottrell <Rylan.Cottrell@Desire2Learn.com>
2013-10-30 11:24:34 -0400

    More Less control, fixed height of breakafter and removed gradient for IE9 as it is poorly supported.

--- moreLess.js
+++ moreLess.js
@@ -1,7 +1,7 @@
 ﻿( function( $ ) {
 
 	$.widget( "vui.vui_moreless", {
-		
+
 		options: {
 			height: '4em',
 			lineHeight: 'normal',
@@ -15,27 +15,27 @@
 			var me = this;
 			var $moreblur = null;
 			var $moreless = $( this.element );
-			var $morelink = $( '<div class="vui-moreless-link vui-link">' );
-			
+			var $morelink = $( '<div class="vui-moreless-link vui-link" tabindex="0">' );
+
 			$moreless.after( $morelink );
-			
-			me.options.title.more = $moreless.attr( 'data-moreless-moretitle' ) !== undefined ? $moreless.attr( 'data-moreless-moretitle' ) : this.options.title.more; 
-			me.options.title.less = $moreless.attr( 'data-moreless-lesstitle' ) !== undefined ? $moreless.attr( 'data-moreless-lesstitle' ) : this.options.title.less; 
-			me.options.accessible = $moreless.attr( 'data-moreless-accessible' ) !== undefined ? true : false;
-			
+
+			me.options.title.more = $moreless.attr( 'data-moreless-moretitle' ) !== undefined ? $moreless.attr( 'data-moreless-moretitle' ) : this.options.title.more;
+			me.options.title.less = $moreless.attr( 'data-moreless-lesstitle' ) !== undefined ? $moreless.attr( 'data-moreless-lesstitle' ) : this.options.title.less;
+			me.options.accessible = $moreless.attr( 'data-moreless-accessible' ) !== undefined && $moreless.attr( 'data-moreless-accessible' ) !== "False"? true : false;
+
 			var lineHeight = $moreless.attr( 'data-moreless-lineHeight' ) !== undefined ? $moreless.attr( 'data-moreless-lineHeight' ) : this.options.lineHeight;
 			$moreless.css( 'line-height', lineHeight );
-	
+
 			$breakafter = $moreless.find( '.vui-moreless-breakafter' );
 			var height = 0;
 
 			if( $breakafter.length ) {
 				$breakafter = $( $breakafter[0] );
 				//determine hieght based on bottom on node with breakafter class
-				height =  $breakafter.get(0).scrollHeight;
-				
+				height = $breakafter.position().top + $breakafter.get( 0 ).scrollHeight;
+
 			} else {
-				height = $moreless.attr( 'data-moreless-height' ) !== undefined ? $moreless.attr( 'data-moreless-height' ) : this.options.height; 
+				height = $moreless.attr( 'data-moreless-height' ) !== undefined ? $moreless.attr( 'data-moreless-height' ) : this.options.height;
 
 				if( height.indexOf( '%' ) > -1 ) { //convert percent to px to prevent loss of transition
 					height = parseInt( ( $moreless.get( 0 ).scrollHeight * ( parseInt( height, 10 ) / 100 ) ) + 0.5, 10 );
