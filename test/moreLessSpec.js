95003449e859c5ec71e76f1377180fa79aafe3cc
Dave Batiste <dave.batiste@d2l.com>
2014-10-29 13:44:38 -0400

    Migrating tests.

--- test/moreLessSpec.js
+++ test/moreLessSpec.js
@@ -5,6 +5,12 @@
 
 		var node, $container1;
 
+		var createKeyEvent = function( eventType, keyCode ) {
+			var e = $.Event( eventType );
+			e.keyCode = keyCode;
+			return e;
+		};
+
 		beforeEach( function () {
 
 			jasmine.addMatchers( d2l.jasmine.matchers );
@@ -50,6 +56,82 @@
 				$container1.vui_moreless( 'destroy' );
 				expect( $container1.data( 'vui-vui_moreless' ) )
 					.not.toBeDefined();
+			} );
+
+		} );
+
+		describe( 'show/hide more', function() {
+
+			beforeEach( function () {
+				$container1.attr( 'data-moreless-moretitle', '+More' );
+				$container1.attr( 'data-moreless-lesstitle', '-Less' );
+				$container1.vui_moreless();
+			} );
+
+			it( 'sets the link text when link is clicked to expand', function() {
+				$( '.vui-moreless-link' ).click();
+				expect( $( '.vui-moreless-link' )[0].innerText ).toBe( '-Less' );
+			} );
+
+			it( 'triggers the vui-moreless-expand event when clicking to show more content', function( done ) {
+				$container1.on( 'vui-moreless-expand', function() {
+					done();
+				} );
+				$( '.vui-moreless-link' ).click();
+			} );
+
+			it( 'triggers the vui-moreless-expand event [enter] key has been pressed to show more', function( done ) {
+				$container1.on( 'vui-moreless-expand', function() {
+					done();
+				} );
+				$( '.vui-moreless-link' ).focus();
+				$( '.vui-moreless-link' ).trigger( createKeyEvent( 'keypress', 13 ) );
+			} );
+
+			it( 'sets the link text when link is clicked to collapse', function() {
+				$( '.vui-moreless-link' ).click();
+				$( '.vui-moreless-link' ).click();
+				expect( $( '.vui-moreless-link' )[0].innerText ).toBe( '+More' );
+			} );
+
+			it( 'triggers the vui-moreless-expand event when clicking to show more content', function( done ) {
+				$container1.on( 'vui-moreless-collapse', function() {
+					done();
+				} );
+				$( '.vui-moreless-link' ).click();
+				$( '.vui-moreless-link' ).click();
+			} );
+
+			it( 'triggers the vui-moreless-expand event [enter] key has been pressed to show more', function( done ) {
+				$container1.on( 'vui-moreless-collapse', function() {
+					done();
+				} );
+				$( '.vui-moreless-link' ).focus();
+				$( '.vui-moreless-link' ).trigger( createKeyEvent( 'keypress', 13 ) );
+				$( '.vui-moreless-link' ).trigger( createKeyEvent( 'keypress', 13 ) );
+			} );
+
+		} );
+
+		describe( 'isExpanded', function() {
+
+			beforeEach( function () {
+				$container1.vui_moreless();
+			} );
+
+			it( 'returns false when less content is displayed', function() {
+				expect( $container1.vui_moreless( 'isExpanded', $container1.get( 0 ) ) ).toBeFalsy();
+			} );
+
+			it( 'returns true when more content is displayed', function() {
+				$( '.vui-moreless-link' ).click();
+				expect( $container1.vui_moreless( 'isExpanded', $container1.get( 0 ) ) ).toBeTruthy();
+			} );
+
+			it( 'returns false when less content is displayed', function() {
+				$( '.vui-moreless-link' ).click();
+				$( '.vui-moreless-link' ).click();
+				expect( $container1.vui_moreless( 'isExpanded', $container1.get( 0 ) ) ).toBeFalsy();
 			} );
 
 		} );
